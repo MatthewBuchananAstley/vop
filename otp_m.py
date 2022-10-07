d985c4a5370d287e2dd997646663ebbd2e16d60f
MatthewBuchananAstley <matthewbuchanan@astley.nl>
2022-10-07 16:48:12 +0200

    added b64 urlsafe encoding option

--- otp_m.py
+++ otp_m.py
@@ -7,15 +7,21 @@ import os,sys
 import random 
 import hashlib
 import otp_t
+import argparse
+import base64
 
-try:
-    pwlen = sys.argv[1]
-    if int(pwlen) < 8:
-        print("Minimum 8 characters, please\n")
+
+def process_opts(a):
+
+    try:
+        pwlen = a 
+        if int(pwlen) < 8:
+            print("Minimum 8 characters, please\n")
+            sys.exit()
+         
+    except IndexError:
+        print("Please input pw length\n")
         sys.exit()
-except IndexError:
-    print("Please input pw length\n")
-    sys.exit()
 
 def print_mijndate():
     from time import gmtime,strftime
@@ -34,13 +40,35 @@ def rhsh(pwlen):
     d1 = d[0:int(pwlen)] 
     return(d1)
 
+def b64enc(a3):
+        try:
+            if int(args.urlsafe_encode) == 1:
+                a4 = base64.urlsafe_b64encode(bytes(a3,encoding='utf-8'))
+                a5 = str(a4).strip('b\'')
+                return(a5)
+        except TypeError:
+            return(a3)
+
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pwl', '--password_length')
+    parser.add_argument('-b64', '--urlsafe_encode')
+    args = parser.parse_args()
+
+    print("Ja",args.password_length)
+    print("Ja",args.urlsafe_encode)
+    pwlen = args.password_length
+    process_opts(pwlen)
+
     a = rhsh(int(pwlen))
+
     if len(a) == 64:
         a2 = a[0:len(a) -1]
         a2 += str(otp_t.rspec_char()) 
         a3 = otp_t.rd(a2)
-        print(a3)
+        print(b64enc(a3))
     else:
         a3 = otp_t.rd(a)
-        print(a3)
+        print(b64enc(a3))
