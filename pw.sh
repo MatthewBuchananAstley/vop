3e749dba9b22aaff560bc39ec24aac2f61b4a857
Matthew Buchanan Astley <matthewbuchanan@astley.nl>
2024-03-07 17:48:18 +0100

    Added explanation text

--- pw.sh
+++ pw.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
-# 2022, 2023 Matthew Buchanan Astley
-# To generate a bunch of passwords 
+# Script to generate a bunch of passwords in a row of random length.
+# The password you subsequently select from that list makes the password even more unpredictable.
+#
+# SPDX-License-Identifier: Apache-2.0
+# FileCopyrightText: <text> 2022-2024 Matthew Buchanan Astley (mbastley how to make this work do you think? gmail.com, matthewbuchanan and this? astley.nl) </text>
+#
+
 
 
 for i in `seq 1 $(shuf -n 1  -i 1-100)`
