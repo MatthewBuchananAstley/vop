1581808fcb19791790dcf71a3adb1d19b4023c60
Matthew Buchanan Astley <matthewbuchanan@astley.nl>
2024-03-20 22:22:18 +0100

    Created a unit test to test for pw length, lowercase and uppercase letter

---
+++ testpw.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import os,sys
+import unittest
+import vop 
+
+
+class TestPwMethods(unittest.TestCase):
+
+    def getpw(self):
+        a = vop.rhsh(int(10))
+        a1 = vop.rd(a)
+        return(vop.apw(a1))
+
+    def test_pw(self):
+        a = self.getpw()
+        uc_cnt = 0
+        lc_cnt = 0 
+
+        for i in a:
+            if i.isupper() == True:
+                 uc_cnt += 1 
+
+        for i in a:
+            if i.islower() == True:
+                 lc_cnt += 1
+
+        err = {}
+
+        err["l_err"] = 0 
+
+        if len(a) == 10:
+            print("Ja 10")
+        else: 
+            print("Fout ", len(a))
+            err["l_err"] += 1
+
+        err["uc_err"] = 0
+
+        if uc_cnt >= 1:
+            print("Ja", uc_cnt, " ", a)
+        else: 
+            #print("fout")
+            err["uc_err"] += 1
+
+        err["lc_err"] = 0 
+
+        if lc_cnt >= 1:
+            print("Ja", lc_cnt, " ", a)
+        else:
+            err["lc_err"] += 1
+
+       
+        for i in err.keys():
+            if err[i] == 0:
+                print("PASS")
+            else:
+                #print("FAIL ", i) 
+                exit("FAIL " + i) 
+      
+if __name__ == '__main__':
+    unittest.main()   
