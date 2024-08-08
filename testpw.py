b2dd10f33517136c32d2d8cfe4f4dfd4f2e38e1f
Matthew Buchanan Astley <matthewbuchanan@astley.nl>
2024-08-08 23:04:56 +0200

    Adjusted testpw.py script

--- testpw.py
+++ testpw.py
@@ -23,42 +23,50 @@ import vop
 class TestPwMethods(unittest.TestCase):
 
     def getpw(self):
-        a = vop.rhsh(int(10))
-        a1 = vop.rd(a)
-        return(vop.apw(a1))
+        #a = vop.rhsh(int(10))
+        #a1 = vop.rd(a)
+        #return(vop.apw(a1))
+        return(vop.chstr.prstr(vop.chstr.rhsh(int(10))))
+
+    def chkspchr(self):
+        spchr = [ '!','@','#','$','%','^','&','*','_','-','+','=','/','?','|'] 
+        if self in spchr:
+            return(True)
 
     def test_pw(self):
         a = self.getpw()
         uc_cnt = 0
         lc_cnt = 0 
         sc_cnt = 0
+        gtl = 0
 
         for i in a:
             if i.isupper() == True:
                 uc_cnt += 1 
 
-        for i in a:
             if i.islower() == True:
                 lc_cnt += 1
 
-        for i in a:
-            if vop.chkspchr(i) == True:
+            if TestPwMethods.chkspchr(i) == True:
                 sc_cnt += 1 
+  
+            if i.isdigit() == True:
+                gtl += 1
 
         err = {}
 
         err["l_err"] = 0 
 
         if len(a) == 10:
-            print("Ja 10")
+            print("Ja aantal karakters: ",len(a))
         else: 
             print("Fout ", len(a))
             err["l_err"] += 1
 
         err["uc_err"] = 0
-
+        
         if uc_cnt >= 1:
-            print("Ja", uc_cnt, " ", a)
+            print("Ja aantal uppercase: ", uc_cnt, " ", a)
         else: 
             #print("fout")
             err["uc_err"] += 1
@@ -66,17 +74,24 @@ class TestPwMethods(unittest.TestCase):
         err["lc_err"] = 0 
 
         if lc_cnt >= 1:
-            print("Ja", lc_cnt, " ", a)
+            print("Ja aantal lowercase: ", lc_cnt, " ", a)
         else:
             err["lc_err"] += 1
 
         err["sc_err"] = 0
 
         if sc_cnt >= 1:
-            print("Ja", sc_cnt, " ", a)
+            print("Ja speciaal karakter: ", sc_cnt, " ", a)
         else:
             err["sc_err"] += 1
-             
+       
+        err["gtl"] = 0 
+ 
+        if gtl >= 1:
+            print("Ja getal: ", gtl, " ", a)
+        else:
+            err["gtl"] += 1 
+              
         for i in err.keys():
             if err[i] == 0:
                 print("PASS")
