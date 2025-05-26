d8294345d02acf58ecca4aba75a7253ff4b6004d
Matthew Buchanan Astley <linux@astley.nl>
2025-05-26 16:02:16 +0200

    Added shellscript to test the returned character number

---
+++ test_vop_1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rfile=$(date '+%Y%m%d%H%M%S')
+pwl=$1
+
+if [ -z $pwl ] ; then
+    echo "Please provide the password length"
+    exit 2
+fi
+
+function cleanr() {
+    echo "SIGINT!" 
+    if [ -e latest_vop_1_results.txt ] ; then
+        rm latest_vop_1_results.txt 
+        ln -s $(ls -larntf results_* | tail -1 | awk '{print $9}') latest_vop_1_results.txt 
+    fi
+    exit 1
+}
+
+trap cleanr SIGINT
+
+while true ; 
+    do 
+        a=$(./vop $pwl); 
+        #a1=$(echo "$a" | sed -z 's/\n//g'| wc -c) ; 
+        a1=$(echo "$a" | tail -1| sed -z 's/\n//g'| wc -c) ; 
+
+        if [ -z $2 ] ; then
+            echo "$a" "$a1" ; 
+        fi 
+           
+
+        if [ $a1 != $pwl ] ; then 
+            echo Ja "$a" ; 
+            echo "JA $pwl $(echo "$a") "$a1" "$a" $(date '+%Y%m%d%H%M%S')" >> results_$pwl_$rfile.txt  ;
+            #rm latest_vop_1_results.txt 
+            #ln -s $(ls -larntf results_* | tail -1 | awk '{print $9}') latest_vop_1_results.txt
+            exit 2
+        fi; 
+    done
